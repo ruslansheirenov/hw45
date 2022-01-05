933b75325ed29da5ec99ad720046fa9be16920e1
sheirenov <ruslansheirenov@gmail.com>
2022-01-05 22:08:16 +0600

    Добавлена функция task_cteate_view, шаблон для создания новой задачи сохранение ее в базу данных

--- source/ToDo/views.py
+++ source/ToDo/views.py
@@ -10,3 +10,15 @@ def index_view(request):
         'tasks': tasks
     }
     return render(request, 'index.html', context)
+
+def task_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'task_create.html')
+    else:
+        title = request.POST.get('title')
+        status = request.POST.get('status')
+        description = request.POST.get('description')
+        new_task = Task.objects.create(title=title, status=status, description=description)
+        context = {'task': new_task}
+
+        return render(request, 'task_view.html', context)
--- source/ToDo_list/urls.py
+++ source/ToDo_list/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ToDo.views import index_view
+from ToDo.views import index_view, task_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view)
+    path('', index_view),
+    path('tasks/add/', task_create_view),
 ]
